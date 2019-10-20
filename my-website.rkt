f16f7fc6c9c31d10245cca414b001af6c975c63d
Lindsey Handley <lindseyhandley@gmail.com>
2019-10-19 17:09:03 -0700

    added my-website

---
+++ my-website.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(require website 
+         (only-in 2htdp/image
+                  circle))
+
+(provide my-site)
+
+
+(define (blog-page title . stuff)
+  (html
+    (body
+      (a 'href: "/" "Back Home")
+      title
+      stuff)))
+
+(define my-site
+  (site
+    (index
+      (page index.html
+            (html
+              (body
+                (h1 "This is Lindsey's Homepage")
+                (link-to blog-post1 
+                         "My Story")))))
+
+    (blog-post1
+      (page blog/first-post.html
+            (blog-page
+              (h1 "This is my story!!")    
+              (write-image (circle 40 'solid 'red)))))
+
+
+;TODO:
+;  * Add missing attributes, src:, href:, etc...
+
+
+
